8305febd843db42a4d3944891eb57b5a178b687a
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-12-07 15:36:53 +0100

    updater: introduce verify_no_downgrade function
    Change-Id: I20a8cac88eaec89b7bddab648852c51cda38b504

--- updater/samsung.cpp
+++ updater/samsung.cpp
@@ -20,6 +20,7 @@
 #include <libgen.h>
 #include <unistd.h>
 
+#include <android-base/logging.h>
 #include <android-base/properties.h>
 #include <edify/expr.h>
 #include <otautil/error_code.h>
@@ -54,6 +55,80 @@ Value* MarkHeaderBtFn(const char* name, State* state,
   close(fd);
 
   return StringValue("t");
+}
+
+Value *VerifyNoDowngradeFn(const char* name, State* state,
+                           const std::vector<std::unique_ptr<Expr>>& argv) {
+  std::vector<std::string> args;
+  std::string bl1;
+  std::string bl2;
+
+  if (argv.size() < 2 || argv.size() > 2 || !ReadArgs(state, argv, &args))
+    return ErrorAbort(state, kArgsParsingFailure,
+                      "%s() error parsing arguments", name);
+
+  bl1 = args[0].c_str();
+  bl2 = args[1].c_str();
+
+  std::string bl1_model = bl1.substr(0, bl1.size() - 8);
+  std::string bl2_model = bl2.substr(0, bl2.size() - 8);
+  if (bl1_model != bl2_model) {
+    LOG(ERROR) << "Mismatched model (" << bl1_model << " vs. " << bl2_model << ")";
+    return StringValue("");
+  }
+
+  std::string bl1_region = bl1.substr(bl1_model.size(), 2);
+  std::string bl2_region = bl2.substr(bl2_model.size(), 2);
+  if (bl1_region != bl2_region) {
+    LOG(ERROR) << "Mismatched region (" << bl1_region << " vs. " << bl2_region << ")";
+    return StringValue("");
+  }
+
+  char bl1_rp = bl1[bl1_model.size() + 3];
+  char bl2_rp = bl2[bl2_model.size() + 3];
+  if (bl1_rp > bl2_rp) {
+    LOG(ERROR) << "Higher rollback bit (" << bl1_rp << " vs. " << bl2_rp << ")";
+    return StringValue("");
+  }
+
+  char bl1_major = bl1[bl1_model.size() + 4];
+  char bl2_major = bl2[bl2_model.size() + 4];
+  if (bl1_major < bl2_major) {
+    return StringValue("t");
+  } else if (bl1_major > bl2_major) {
+    LOG(ERROR) << "Higher OS major version (" << bl1_major << " vs. " << bl2_major << ")";
+    return StringValue("");
+  }
+
+  char bl1_year = bl1[bl1_model.size() + 5];
+  char bl2_year = bl2[bl2_model.size() + 5];
+  if (bl1_year < bl2_year) {
+    return StringValue("t");
+  } else if (bl1_year > bl2_year) {
+    LOG(ERROR) << "Higher OS year build (" << bl1_year << " vs. " << bl2_year << ")";
+    return StringValue("");
+  }
+
+  char bl1_month = bl1[bl1_model.size() + 6];
+  char bl2_month = bl2[bl2_model.size() + 6];
+  if (bl1_month < bl2_month) {
+    return StringValue("t");
+  } else if (bl1_month > bl2_month) {
+    LOG(ERROR) << "Higher OS month build (" << bl1_month << " vs. " << bl2_month << ")";
+    return StringValue("");
+  }
+
+  char bl1_minor = bl1[bl1_model.size() + 7];
+  char bl2_minor = bl2[bl2_model.size() + 7];
+  if (bl1_minor < bl2_minor) {
+    return StringValue("t");
+  } else if (bl1_minor > bl2_minor) {
+    LOG(ERROR) << "Higher OS minor version (" << bl1_minor << " vs. " << bl2_minor << ")";
+    return StringValue("");
+  }
+
+  LOG(ERROR) << "End reached (" << bl1 << " vs. " << bl2 << ")";
+  return StringValue("");
 }
 
 #define FILENAME_MAX_LEN 32
@@ -108,5 +183,6 @@ Value* WriteDataBtFn(const char* name, State* state,
 
 void RegisterSamsungFunctions() {
   RegisterFunction("mark_header_bt", MarkHeaderBtFn);
+  RegisterFunction("verify_no_downgrade", VerifyNoDowngradeFn);
   RegisterFunction("write_data_bt", WriteDataBtFn);
 }
